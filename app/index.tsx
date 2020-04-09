252c7c5b554bb9bd2c294e10340aff5436f63972
Matthew1994 <minghaoliao1994@gmail.com>
2020-04-09 21:27:11 +0800

    添加B端构建流程

--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,13 @@ const store = configureStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
+// B/C 架构下， 开发模式下用 HtmlWebpackPlugin 创建 html 的文件是 root div 标签的
+if (!document.getElementById('root')) {
+  const root = document.createElement('div');
+  root.id = 'root';
+  document.querySelector('body')!.appendChild(root);
+}
+
 document.addEventListener('DOMContentLoaded', () =>
   render(
     <AppContainer>
